8a6a6eee897e479e44d63d753b0f5bf8a31046e1
Charlie <weberscharles@gmail.com>
2017-11-09 20:17:52 -0600

    create normalizer function

--- js/app.js
+++ js/app.js
@@ -1,73 +1,85 @@
 $(document).ready(function() {
-// var url = 'https://accesscontrolalloworiginall.herokuapp.com/http://mashable.com/stories.json';
-var url = 'https://accesscontrolalloworiginall.herokuapp.com/http://mashable.com/stories.json';
-// var digg = 'http://digg.com/api/news/popular.json';
-// var reddit = 'https://www.reddit.com/top.json';
+	var url = 'https://accesscontrolalloworiginall.herokuapp.com/http://mashable.com/stories.json';
+	// var url = 'http://digg.com/api/news/popular.json';
+	// var url = 'https://www.reddit.com/top.json';
 
-function normalizer(input, url) {
-	var article = {};
-	switch(input) {
-		// if url is for mashable, assign it to a variable and switch on that variable
-		case url = url: 
-			
-			break;
-		// if url is for digg, assign it to a varaible and switch on that variable
-		case digg:
-			
-			break;
-		// if url is for reddit, assign it to a variable and switch on that variable
-		// case api3:
-		// 	do this;
-		// 	break;
-		default:
-			console.log("not working"); 
-	};
-}
-
-function apiCall(url) {
-	$.ajax({
-	url: url,
-	success: function(result){
-		var newArticle = result.new;
-		console.log(newArticle[0].title);
-		for (var i = 0; i < result.new.length; i++) {
-			var newArticleTitle = result.new.title;
-			console.log(newArticle[i]);
-			$('#main').append(formatArticle(result.new));
-		}
-		console.log(result);
+// what is input?
+	function normalizer(input, url) {
+		var article = {};
+		switch(input) {
+			// if url is for mashable, assign it to a variable and switch on that variable
+			case url = url: 
+				article = result.new;
+				// am i calling apiCall here and in the same place in the next two functions?
+				break;
+			// if url is for digg, assign it to a varaible and switch on that variable
+			case digg = url:
+				article = result.data.feed[i].content;
+				break;
+			// if url is for reddit, assign it to a variable and switch on that variable
+			case reddit = url:
+				article = result.data.children[i].data;
+				break;
+			default:
+				console.log("doesn't look like the URL used has any significance here."); 
+		};
 	}
-	});
-}
 
-console.log("starting ajax");
-apiCall(url);
-		// if i want to use the hot articles and rising articles, are those separate Ajax requests?
-	 
-function formatArticle(article) {
-	var result = '<article class="article">' + 
-        '<section class="featuredImage">' + 
-        '<img src="' + article.image + '" alt="" />' + 
-        '</section>' +
-        '<section class="articleContent">' +
-            '<a href="#"><h3>' + article.title + '</h3></a>' + 
-            // '<h5>' + newArticle[i].excerpt + '</h5>' +
-            '<h6>' + article.channel + '</h6>' +
-        '</section>' +
-        '<section class="impressions">' + 
-          
-        '</section>' + 
-        '<div class="clearfix"></div>' +
-      '</article>';
-      return result;
-} 
+	function apiCall(url) {
+		$.ajax({
+		url: url,
+		success: function(result){
+			// normalizer(input, url);
+			var newArticle = result.new;
+			// console.log(newArticle);
+			// console.log(result.new.title);
+			for (var i = 0; i < result.new.length; i++) {
+				console.log(result.new[i]);
+				$('#main').append(formatArticle(newArticle));
+			}
+			console.log(result);
+		}
+		});
+	}
 
-      // $('.articleContent a').click(function() {
-      // 	$('#popUp.loader .container').css("display", "inline");
-      // })
+	// 	function apiCall(url) {
+	// 	$.ajax({
+	// 	url: url,
+	// 	success: function(result){
+	// 		normalizer(input, url);
+	// 		var newArticle = result.new;
+	// 		console.log(result.new.title);
+	// 		for (var i = 0; i < article.length; i++) {
+	// 			console.log(article[i]);
+	// 			$('#main').append(formatArticle(article));
+	// 		}
+	// 		console.log(result);
+	// 	}
+	// 	});
+	// }
 
-
-      
+	console.log("starting ajax");
+	apiCall(url);
+		 
+	function formatArticle(article) {
+		// update newArticle with article
+		var result = '<article class="article">' + 
+	        '<section class="featuredImage">' + 
+	        '<img src="' + article.image + '" alt="" />' + 
+	        '</section>' +
+	        '<section class="articleContent">' +
+	            '<a href="#"><h3>' + article.title + '</h3></a>' + 
+	            // '<h5>' + newArticle[i].excerpt + '</h5>' +
+	            '<h6>' + article.channel + '</h6>' +
+	        '</section>' +
+	        '<section class="impressions">' + 
+	          
+	        '</section>' + 
+	        '<div class="clearfix"></div>' +
+	      '</article>';
+	      return result;
+	} 
+	      
 });
 
 
