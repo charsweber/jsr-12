aed523fda1984e960eeec3cc7f0f0263afa98850
Charlie <weberscharles@gmail.com>
2017-11-12 14:49:05 -0600

    adding branch

--- js/app.js
+++ js/app.js
@@ -1,97 +1,120 @@
 $(document).ready(function() {
-	var url = 'https://accesscontrolalloworiginall.herokuapp.com/http://mashable.com/stories.json';
-	// var url = 'http://digg.com/api/news/popular.json';
-	// var url = 'https://www.reddit.com/top.json';
+	// how do i pass these in dynamically?
+	var urls = ['https://accesscontrolalloworiginall.herokuapp.com/http://mashable.com/stories.json', 'https://accesscontrolalloworiginall.herokuapp.com/http://digg.com/api/news/popular.json', 'https://accesscontrolalloworiginall.herokuapp.com/https://www.reddit.com/top.json'];
 
-// what is input?
 	function normalizer(input, url) {
-		var article = {};
-		switch(input) {
-			// if url is for mashable, assign it to a variable and switch on that variable
-			case url = url: 
-				article = result.new;
-				// am i calling apiCall here and in the same place in the next two functions?
+		var articles = {};
+		(function(input) {
+			switch(url) {
+			case urls[0]: 
+				// i should be populating articles object with json data?
+				articles = input.new;
+				for (var i = 0; i < articles.length; i++) {
+					var image = input.new[i].image;
+					var title = input.new[i].title;
+					var channel = input.new[i].channel;
+					articles[url] = {};
+					articles[url].image = image;
+					articles[url].title = title;
+					articles[url].channel = channel;
+					console.log(articles[url], "articles");
+				}
+				return articles;
 				break;
-			// if url is for digg, assign it to a varaible and switch on that variable
-			case digg = url:
-				article = result.data.feed[i].content;
+			case urls[1]:
+				articles = input.data.feed;
+				for (var i = 0; i < articles.length; i++) {
+					var image = input.data.feed[i].content.media.images[0].original_url;
+					var title = input.data.feed[i].content.title;
+					var channel = input.data.feed[i].content_type;
+					articles[url] = {};
+					articles[url].image = image;
+					articles[url].title = title;
+					articles[url].channel = channel;
+					console.log(articles[url], "articles");
+				}
+				return articles;
 				break;
-			// if url is for reddit, assign it to a variable and switch on that variable
-			case reddit = url:
-				article = result.data.children[i].data;
+			case urls[2]:
+				articles = input.data.children;
+				for (var i = 0; i < articles.length; i++) {
+					// why won't source url work
+					var image = input.data.children[i].data.thumbnail;
+					var title = input.data.children[i].data.title;
+					var channel = input.data.children[i].data.subreddit;
+					articles[url] = {};
+					articles[url].image = image;
+					articles[url].title = title;
+					articles[url].channel = channel;
+					console.log(articles[url], "articles");
+				}
+				return articles;
 				break;
 			default:
-				console.log("doesn't look like the URL used has any significance here."); 
-		};
+				console.log("The URL used has no significance here."); 
+		}})(input);
+		// console.log(articles, "new articles");
+		return articles;
 	}
+
+	console.log("starting ajax");
 
 	function apiCall(url) {
 		$.ajax({
-		url: url,
+		url: urls[i],
 		success: function(result){
-			// normalizer(input, url);
-			var newArticle = result.new;
-			// console.log(newArticle);
-			// console.log(result.new.title);
-			for (var i = 0; i < result.new.length; i++) {
-				console.log(result.new[i]);
-				$('#main').append(formatArticle(newArticle));
+			var article = normalizer(result, url);
+			for (var i = 0; i < article.length; i++) {
+				console.log(article[i]);
+				$('#main').append(formatArticle(article[i]));
 			}
-			console.log(result);
 		}
 		});
 	}
 
-	// 	function apiCall(url) {
-	// 	$.ajax({
-	// 	url: url,
-	// 	success: function(result){
-	// 		normalizer(input, url);
-	// 		var newArticle = result.new;
-	// 		console.log(result.new.title);
-	// 		for (var i = 0; i < article.length; i++) {
-	// 			console.log(article[i]);
-	// 			$('#main').append(formatArticle(article));
-	// 		}
-	// 		console.log(result);
-	// 	}
-	// 	});
-	// }
-
-	console.log("starting ajax");
-	apiCall(url);
+	
+	for (var i = 0; i < urls.length; i++) {
+		apiCall(urls[i]);
+	}
 		 
 	function formatArticle(article) {
-		// update newArticle with article
-		var result = '<article class="article">' + 
+			var result = '<article class="article">' + 
 	        '<section class="featuredImage">' + 
 	        '<img src="' + article.image + '" alt="" />' + 
 	        '</section>' +
 	        '<section class="articleContent">' +
 	            '<a href="#"><h3>' + article.title + '</h3></a>' + 
-	            // '<h5>' + newArticle[i].excerpt + '</h5>' +
 	            '<h6>' + article.channel + '</h6>' +
 	        '</section>' +
 	        '<section class="impressions">' + 
 	          
 	        '</section>' + 
 	        '<div class="clearfix"></div>' +
-	      '</article>';
+	      	'</article>';
+		
 	      return result;
+
+	 //    $('.articleContent h3').click(function() {
+		// 	$(this).attr('id', 'popUp');
+		// 	var popUpContent = '<h1>' + article.title + '</h1>' + 
+		// 		'<p>' + article.content + '</p>' +
+		// 		'<a href="' + article.link + '#" class="popUpAction" target="_blank"> Read more from source </a>';
+		// });  
 	} 
-	      
+
+	
+	
+	$('#search img').click(function() {
+		if ($('#search').hasClass('active')) {
+			$('#search').removeClass('active');
+		}
+		else {
+			$('#search').attr('class', 'active');
+		}
+	});
+
 });
 
-
-
-/*
-When the user selects an article's title show the #popUp overlay. 
-The content of the article must be inserted in the .container class inside #popUp. 
-Make sure you remove the .loader class when toggling the article information in the pop-up.
-
-do this by using a click event to show the #popUp overlay
-get rid of #popUp.loader .container hiding popup by removing .loader?
-*/
 
 
 
